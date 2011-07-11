7ce2ae484c1d3f70a468be4e74acbfe3ebba97f7
danzel <danzel@localhost.geek.nz>
2011-07-11 15:39:02 +1200

    Add powershell dependency and bump version

--- iis7/metadata.rb
+++ iis7/metadata.rb
@@ -1,6 +1,8 @@
-maintainer       "YOUR_COMPANY_NAME"
-maintainer_email "YOUR_EMAIL"
+maintainer       "Smartrak"
+maintainer_email "dave@smartrak.co.nz"
 license          "All rights reserved"
 description      "Installs/Configures iis7"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.0.1"
+version          "0.0.2"
+
+depends "powershell"
